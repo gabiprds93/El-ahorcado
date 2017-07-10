a7312f40df65673eb16a041a41cabe0860038160
gabiprds93 <gabiprds.93@gmail.com>
2017-07-10 15:55:22 -0500

    Quitar funcion innecesaria

--- ahorcado.js
+++ ahorcado.js
@@ -1,12 +1,3 @@
-function imprimirHTML(M)
-{
-    var n = M.length;
-    for (var i = 0; i < n; i++)
-    {
-        console.log(M[i]); 
-    }  
-} 
-
 function imprimirMatriz(M)
 {
     var n = M.length;
